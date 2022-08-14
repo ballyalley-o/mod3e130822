cfc998c0e146f21638df45cef844e5cfb7c7606b
bally <108769285+ballyalley-o@users.noreply.github.com>
2022-08-14 23:51:35 +1200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,99 @@
+const companies = [ 
+
+    { name: "Company One", category: "Finance", start: 1981, end: 2004 }, 
+  
+    { name: "Company Two", category: "Retail", start: 1992, end: 2008 }, 
+  
+    { name: "Company Three", category: "Auto", start: 1999, end: 2007 }, 
+  
+    { name: "Company Four", category: "Retail", start: 1989, end: 2010 }, 
+  
+    { name: "Company Five", category: "Technology", start: 2009, end: 2014 }, 
+  
+    { name: "Company Six", category: "Finance", start: 1987, end: 2010 }, 
+  
+    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 }, 
+  
+    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 }, 
+  
+    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 }, 
+  
+  ];
+  
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32]; 
+
+ 
+// console.log ('Exercise 1')
+// companies.forEach((companies) => {
+//     console.log(companies.name)})
+
+// console.log ('Exercise 2');
+// companies.forEach((company) => {
+//   if(company.start >= 1987);
+ 
+  
+//   console.log(company.name);
+
+// }
+// );
+
+//random exercis by Jason
+// const sortThing = companies.sort(function(a, b) {
+//   return a.start - b.start});
+
+//   console.log(sortThing)
+
+// console.log ('Exercise 3');
+
+// const filtered = companies.filter(company => 
+//   company.category === "Retail") ;
+
+//   console.log(filtered);
+  
+
+// console.log ('Exercise 4');
+
+// const sortA = companies.sort(function(a, b) {
+//   return a.end - b.end
+// });
+
+// console.log(sortA);
+
+// console.log ('Exercise 5');
+
+// const sortD = ages.sort((a, b) => {
+//   return b - a});
+
+//   console.log(sortD);
+
+// console.log ('Exercise 6');
+
+// let companyTen = companies[0]
+// if (companies[0].name && companies[0].category) {
+//   let { name, category } = companyTen;
+//   let newObj = { name: "CompanyBally", category: "Design" };
+
+//   companies.push(newObj);
+//   console.log(companies)
+// }
+
+// // my interpretation
+// let { name, category } = companies[0];
+
+// console.log(name);
+// console.log(category);
+
+
+
+// //printing Name
+// function printName ({ name, category }) {
+//   console.log(`the name is ${name}`);
+// }
+
+// printName(companies[0]);
+
+
+
+// console.log ('Exercise 7');
+
+
